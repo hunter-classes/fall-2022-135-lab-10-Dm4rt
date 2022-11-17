acdaa8f39f710fb4024894643a002791c535805c
Dm4rt <danmartinez2004@gmail.com>
2022-11-17 08:44:21 -0500

    finished task A

--- funcs.cpp
+++ funcs.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 #include "funcs.h"
 
+int minutesSinceMidnight(Time time){
+  int total = time.h*60;
+  total = total + time.m;
+  return total;
+}
 
+int minutesUntil(Time earlier, Time later){
+  int dist = minutesSinceMidnight(later)-minutesSinceMidnight(earlier);
+  return dist;
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "funcs.h"
 
 int main(){
-
+  std::cout<<minutesSinceMidnight({10, 30})<<" minutes"<<std::endl;
+  std::cout<<minutesUntil( {10, 30}, {13, 40} )<<" minutes"<<std::endl;
   return 0;
 }
